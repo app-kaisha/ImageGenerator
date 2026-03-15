65e4f4e27f5d7ed5cdbee8eb9a3ddfe08ce99942
app-kaisha <app.kaihatsusha@gmail.com>
2026-03-15 19:08:42 +0000

    Initial Commit

---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,18 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by app-kaihatsusha on 15/03/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
