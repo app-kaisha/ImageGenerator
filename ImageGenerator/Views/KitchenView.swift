bb3f91b11c3d78a5318870de316d45c35a480ed0
app-kaisha <app.kaihatsusha@gmail.com>
2026-03-15 20:31:15 +0000

    Added View to display generated image and reset button to exit to main menu

---
+++ ImageGenerator/Views/KitchenView.swift
@@ -0,0 +1,19 @@
+//
+//  KitchenView.swift
+//  ImageGenerator
+//
+//  Created by app-kaihatsusha on 15/03/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct KitchenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KitchenView()
+}
